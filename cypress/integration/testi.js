fddee2bed0643e0d9b17ba243438eac935284695
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-02-03 18:11:41 +0200

    more tests

--- cypress/integration/testi.js
+++ cypress/integration/testi.js
@@ -3,5 +3,10 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('one can navigate from the main page to the page of a particular pokemon, ivysaur', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')
   })
 })
